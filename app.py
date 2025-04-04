b056459d025df612be0151c2f647328cab279062
Andres2908 <saavedratrejo24@gmail.com>
2025-04-03 23:05:47 -0600

    add

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, jsonify
-from flask_cors import CORS  
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    
+
     @app.route('/')
     def home():
-        return jsonify({"mensaje": "Hola, soy una API de python construida con Flask y paso por un proceso de CI/CD!!"})
-    
+        return jsonify({"mensaje": "Hola, soy una API de python construida con Flask y paso por un proceso de CI/CD!!!"})
+
     @app.route('/siayec')
     def siayec():
         return jsonify({"saludo": "Hola grupo Siayec"})
-    
+
     @app.route('/status')
     def status():
         return jsonify({"status": "OK", "version": "1.0"})
-    
+
     return app
 
-app = create_app()
 
+app = create_app()
